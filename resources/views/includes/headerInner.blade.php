ba2577b0b4f76697ce64303ed4c65adfd48a998b
ShroukMohamedAbdelhamied <shroukmousa2017@gmail.com>
2024-06-06 18:16:28 +0300

    modify vedio image

--- resources/views/includes/headerInner.blade.php
+++ resources/views/includes/headerInner.blade.php
@@ -17,24 +17,24 @@
 								<div class="main-menu">
 									<nav class="navigation">
 										<ul class="nav menu">
-										<li class="{{request()->is('/')?"active" : ""}}"><a href="{{route('home')}}">Home <i class="icofont-rounded-down"></i></a>												
+										<li class="{{request()->is('/') ? 'active' : ''}}"><a href="{{route('home')}}">Home <i class="icofont-rounded-down"></i></a>												
 										<ul class="dropdown">
-										<li class="{{request()->is('/')?"active" : ""}}"><a href="{{route('portfolio')}}">Portfolio</a></li>
+										<li class="{{request()->is('/') ? 'active' : ''}}"><a href="{{route('portfolio')}}">Portfolio</a></li>
 												</ul>
 											</li>
-											<li class="{{request()->is('doctor')?"active" : ""}}"><a href="{{route('doctor')}}">Doctors</a></li>
-											<li class="{{request()->is('service')?"active" : ""}}"><a href="{{route('service')}}">Services </a></li>											
-											<li class="{{request()->is('error')?"active" : ""}}"><a href="{{route('error')}}">Pages <i class="icofont-rounded-down"></i></a>
+											<li class="{{request()->is('doctor') ? 'active' : ''}}"><a href="{{route('doctor')}}">Doctors</a></li>
+											<li class="{{request()->is('service') ? 'active': ''}}"><a href="{{route('service')}}">Services </a></li>											
+											<li class="{{request()->is('error') ? 'active': ''}}"><a href="{{route('error')}}">Pages <i class="icofont-rounded-down"></i></a>
 												<ul class="dropdown">
-												<li class="{{request()->is('error')?"active" : ""}}"><a href="{{route('error')}}">404 Error</a></li>
+												<li class="{{request()->is('error') ? 'active': ''}}"><a href="{{route('error')}}">404 Error</a></li>
 												</ul>
 											</li>
-											<li class="{{request()->is('blog')?"active" : ""}}"><a href="{{route('blog')}}">Blogs<i class="icofont-rounded-down"></i></a>				
+											<li class="{{request()->is('blog') ? 'active': ''}}"><a href="{{route('blog')}}">Blogs<i class="icofont-rounded-down"></i></a>				
 											<ul class="dropdown">
-											<li class="{{request()->is('blog')?"active" : ""}}"><a href="{{route('blog')}}">Blog Details</a></li>
+											<li class="{{request()->is('blog') ? 'active' : ''}}"><a href="{{route('blog')}}">Blog Details</a></li>
 												</ul>
 											</li>
-											<li class="{{request()->is('contact')?"active" : ""}}"><a href="{{route('contact')}}">Contact Us</a></li>
+											<li class="{{request()->is('contact') ? 'active' : ''}}"><a href="{{route('contact')}}">Contact Us</a></li>
 										</ul>
 									</nav>
 								</div>
